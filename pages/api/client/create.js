00133555d0f6f124891938e580abe5bafa110143
Johan Alves <johanvaf@gmail.com>
2024-05-03 18:25:12 -0300

    ADS: 01587621 REST-cli

---
+++ pages/api/client/create.js
@@ -0,0 +1,16 @@
+/**
+ * Nome do arquivo: api/client/create.js
+ * Data de criação: 03/05/2024
+ * Autor: Johan Victor
+ * Matrícula: 01587621
+ *
+ * Descrição:
+ * Este arquivo JavaScript é responsável por implementar as funcionalidades
+ * de criação do usuário no banco de dados
+ *
+ * Este script é parte o curso de ADS.
+ */
+
+export default function handler(req, res) {
+  res.status(200).json({ message: "Criar Cliente" });
+}
